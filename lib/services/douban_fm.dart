05e76f7e7add7c2b1d5ed5b7aec886893aa2380e
4wangyu <4wangyu@gmail.com>
2020-04-25 22:54:37 +0800

    perfect fm player

--- lib/services/douban_fm.dart
+++ lib/services/douban_fm.dart
@@ -9,7 +9,11 @@ class DoubanFm {
       'https://api.douban.com/v2/fm/playlist?channel=-10&kbps=128&app_name=radio_android&version=100&type=n';
   final String defaultSong = 'Carla-Bruni-You-Belong-To-Me.mp3';
 
+  // for playing douban fm
   static AudioPlayer audioPlayer;
+  Song nextSong;
+
+  // for playing default song
   static AudioCache cachePlayer = AudioCache();
   AudioPlayer cachePlayerControl;
 
@@ -18,33 +22,22 @@ class DoubanFm {
     audioPlayer.onPlayerCompletion.listen((event) {
       play();
     });
+    _prefetchNextSong();
 
     cachePlayer.load(defaultSong);
   }
 
   play() {
-    http.get(fmPlaylistUrl).then((res) {
-      if (res.statusCode == 200) {
-        var body = jsonDecode(res.body);
-        List songList = body['song'];
+    // stop playing if another alarm already went off
+    stop();
 
-        if (songList.length > 0) {
-          var song = songList[0];
-          String artist = song['artist'];
-          String title = song['title'];
-          print('Playing: $artist - $title');
-          audioPlayer.play(song['url']);
-        } else {
-          throw ('Song list is empty.');
-        }
-      } else {
-        throw ('Http error response.');
-      }
-    }).catchError((e) {
-      print("Got error: ${e.error}");
+    if (nextSong != null) {
+      audioPlayer.play(nextSong.url);
+      print('Playing: ${nextSong.artist} - ${nextSong.title}');
+      _prefetchNextSong();
+    } else {
       _playDefaultSong();
-      return 42; // Future completes with 42.
-    });
+    }
   }
 
   stop() {
@@ -60,4 +53,41 @@ class DoubanFm {
       cachePlayerControl = control;
     });
   }
+
+  _prefetchNextSong() {
+    http.get(fmPlaylistUrl).then((res) {
+      if (res.statusCode == 200) {
+        var body = jsonDecode(res.body);
+        List songList = body['song'];
+
+        if (songList.length > 0) {
+          var song = songList[0];
+          String artist = song['artist'];
+          String title = song['title'];
+          String url = song['url'];
+          nextSong = Song(artist, title, url);
+          print('Prefetched: ${nextSong.artist} - ${nextSong.title}');
+        } else {
+          throw ('Song list is empty.');
+        }
+      } else {
+        throw ('Http error response.');
+      }
+    }).catchError((e) {
+      print("Got error: ${e.error}");
+      return 42; // Future completes with 42.
+    });
+  }
+}
+
+class Song {
+  String title;
+  String artist;
+  String url;
+
+  Song(
+    this.title,
+    this.artist,
+    this.url,
+  );
 }
